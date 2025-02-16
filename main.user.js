00b4112cd35bc1a76456e8a8d67a40a3f001ad0d
qxs <qxs8602@gmail.com>
2025-02-16 23:31:37 +0900

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      1.2
 // @description  사용자가 등록한 아프리카TV 방송인의 방송 상태를 확인하여 알림을 제공합니다.
-// @author       YourName
+// @author       qxs
 // @match        play.sooplive.co.kr
 // @match        www.sooplive.co.kr
 // @match        vod.sooplive.co.kr
